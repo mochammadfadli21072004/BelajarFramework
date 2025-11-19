544aac9509f9b6634a40c1afa454716f5cad7e35
mochammadfadli21072004 <2310631170161@student.unsika.ac.id>
2025-11-20 00:44:08 +0700

    pertemuan 5 Membuat view blade di Laravel (bagian 1)

---
+++ resources/views/barang.blade.php
@@ -0,0 +1,9 @@
+@extends('utama')
+
+@section('judul_menu')
+        ini ditampilkan dari section judul menu, dengan data : {{ $data }} <br>    
+@endsection
+
+@section('isi_menu')
+        ini isi dari section isi menu
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Barang;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/iloveyou', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    echo "julia galak";
+});
+
+Route ::view('/panggil_view','welcome');
+
+Route::redirect('/alihkan','/');
+
+Route::get('/parameter/{id}', function ($id) {
+    echo "Julia galak" .$id;
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/parameter/{id}', function ($id) {
+    echo "Julia galak" .$id;
+    });
+
+    Route::get('/Dasboard', function () {
+    echo "ini Dasboard";
+    });
+});
+
+Route::get('/route_cont/{id}',[Barang::class, 'index']); 
